e9e8a26ac335901d1eb362c5673c528369253b92
PrithwirajPaul <122010113+PrithwirajPaul@users.noreply.github.com>
2024-09-22 09:46:41 +0600

    Update database.php

--- database.php
+++ database.php
@@ -1,32 +1,26 @@
-<!-- manually create your user (name,pass) in this page on 'info_table' -->
 <?php
-$servername = "localhost"; // Replace with your server name
-$username = "root"; // Replace with your MySQL username
-$password = ""; // Replace with your MySQL password
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
 $dbname = "social_database";
 
      // Create connection
      $conn = new mysqli($servername, $username, $password);
  
-     // Check connection
     if ($conn->connect_error) {
         die("DB Connection failed: " . $conn->connect_error);
     } 
     
-    // Create database
     $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
     if ($conn->query($sql) === TRUE) {
     } else {
         echo "Error creating database: " . $conn->error;
     }
 
-    // Close connection to create a new one for the database
     $conn->close();
 
-    // Create connection to the new database
     $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
